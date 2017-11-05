712889c1f3848e464a8f56e7b5d96bc96b644a8e
Amit <amit.akgmca@gmail.com>
2017-11-05 20:41:47 +0530

    api added

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ var app=express();
 app.get('/',function(req,res){
     res.send('Express is working!!');
   });
+
+app.get('/api/test',function(req,res){
+    res.send('My first api on heroku');
+});
+
 app.listen(port,()=>{
     console.log('Running');
 })
